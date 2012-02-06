a2ffa5703142ac9092bb996361b042448d5df05e
kuahyeow <kuahyeow@gmail.com>
2012-02-07 12:44:11 +1300

    Hurray for specs

--- lib/harvester/time_entry.rb
+++ lib/harvester/time_entry.rb
@@ -3,14 +3,10 @@ module Harvester
     API_FIELDS = [:spent_at, :hours, :notes, :id, :project_id, :task_id, :user_id]
     API_FIELDS.each {|field| attr_accessor field}
 
-    def initialize(fields)
+    def initialize(fields = {})
       API_FIELDS.each do |accessor|
-        send "#{accessor}=", fields[accessor.to_s]
+        send "#{accessor}=", fields[accessor]
       end
-    end
-
-    def inspect
-      "#{hours} hours spent at #{spent_at}"
     end
   end
 end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,2 @@
+require File.expand_path('../../lib/harvester', __FILE__)
+
